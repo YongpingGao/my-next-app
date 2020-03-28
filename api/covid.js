60716abe024d931d5d11dee61e009f7a7d90223b
YongpingGao <gaoyongping1990@gmail.com>
2020-03-28 12:13:01 -0400

    add telegram bot

--- api/covid.js
+++ api/covid.js
@@ -1,13 +1,9 @@
-import fetch from "isomorphic-unfetch";
+import bot from "../src/service/telegramApi";
 module.exports = async (req, res) => {
-  console.log("GOT THE DATA", req.body);
-
-  res.status(200).send(req.body);
-
-  //   const promise = await fetch(
-  //     "https://api.covid19api.com/dayone/country/canada/status/confirmed"
-  //   );
-  //   const data = await promise.json();
-
-  //   res.status(200).send(data.filter(country => country.Province === "Ontario"));
+  console.log("GOT THE DATA", req);
+  const data = await bot.sendMessage(
+    "@bayareanews",
+    JSON.stringify(req.body || {})
+  );
+  res.status(200).send("success");
 };
